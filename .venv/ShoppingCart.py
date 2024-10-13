69f26320151fb072e64c27ab3d490faf7aa37271
Richard Anderson <richard.anderson2@csuglobal.edu>
2024-10-13 13:08:08 -0500

    version 2.0

--- .venv/ShoppingCart.py
+++ .venv/ShoppingCart.py
@@ -2,7 +2,8 @@
 #Attributes  item_name (string)  item_price (float)  item_quantity (int)
 #Default constructor Initializes item's name = "none", item's price = 0, item's quantity = 0
 class ItemToPurchase:
-    def __init__(self, item_name = "none", item_price = 0.0, item_quantity = 0, item_total = 0.0):
+    def __init__(self, item_name = "none", item_description = "none", item_price = 0.0, item_quantity = 0,
+                 item_total = 0.0):
         self.item_name = item_name
         self.item_price = item_price
         self.item_quantity = item_quantity
@@ -11,7 +12,7 @@ class ItemToPurchase:
 #Method print_item_cost()
     def print_item_cost(self):
         print(self.item_name + " " + '{:d}'.format(self.item_quantity) + " @ $" +
-              '{:.2f}'.format(self.item_price) + " = $" + '{:.2f}'.format(self.item_total) + "\n")
+              '{:.2f}'.format(self.item_price) + " = $" + '{:.2f}'.format(self.item_total))
 
 #Method find_item_total()
     def find_item_total(self):
@@ -24,7 +25,8 @@ class ItemToPurchase:
 #        current_date (string) - Initialized in default constructor to "January 1, 2020"
 #        cart_items (list)
 class ShoppingCart:
-    def __init__(self, customer_name = "none", current_date = "January 1, 2020", cart_items = [], cart_qty = 0, cart_total = 0.0):
+    def __init__(self, customer_name = "none", current_date = "January 1, 2020", cart_items = [], cart_qty = 0,
+                 cart_total = 0.0):
         self.customer_name = customer_name
         self.current_date = current_date
         self.cart_items = cart_items
@@ -55,7 +57,8 @@ class ShoppingCart:
     def modify_item(self, ItemToPurchase):
         didChange = False
         makeChange = True
-        if ItemToPurchase.item_name == "none" and ItemToPurchase.item_price == 0.0 or ItemToPurchase.item_quantity == 0:
+        if (ItemToPurchase.item_name == "none" and ItemToPurchase.item_name == "none" and
+                ItemToPurchase.item_price == 0.0 or ItemToPurchase.item_quantity == 0):
             makeChange = False
             didChange = True
             print('Default values passed, nothing updated')
@@ -83,58 +86,186 @@ class ShoppingCart:
          for i in self.cart_items:
              self.cart_total += i.item_price * i.item_quantity
 
+#Method print_total()
+    def print_total(self):
+        print(self.customer_name + "'s Shopping Cart - " + self.current_date)
+        self.get_num_items_in_cart()
+        print('Number of Items:', self.cart_qty)
+        for i in self.cart_items:
+            i.find_item_total()
+            i.print_item_cost()
+        self.get_cost_of_cart()
+        print('Total: $' + '{:.2f}'.format(self.cart_total))
+
+#Method print_descriptions()  Outputs each item's description.
+    def print_descriptions(self):
+        print(self.customer_name + "'s Shopping Cart - " + self.current_date)
+        print("Item Descriptions")
+        for i in self.cart_items:
+            print(i.item_name + ": "+ i.item_description)
+
+def print_menu(ShoppingCart):
+    menuInput = ""
+    while menuInput != "q":
+        print("MENU")
+        print("a - Add item to cart")
+        print("r - Remove item from cart")
+        print("c - Change item quantity")
+        print("i - Output items' descriptions")
+        print("o - Output shopping cart")
+        print("q - Quit")
+        print("Choose an option:")
+        menuInput = input()
+
+        if menuInput == "a":
+            #ItemToPurchase.item_name = "none"
+            #ItemToPurchase.item_description = "none"
+            #ItemToPurchase.item_price = 0.0
+            #ItemToPurchase.item_quantity = 0
+            itemEntry = ItemToPurchase()
+            input_item(itemEntry)
+            ShoppingCart.add_item(itemEntry)
+
+        elif menuInput == "r":
+            itemAddRemove = input('Enter Item to Remove: ')
+            ShoppingCart.remove_item(itemAddRemove)
+            print()
+
+        elif menuInput == "c":
+            print("Change Item Quantity in Cart")
+
+        elif menuInput == "i":
+            ShoppingCart.print_descriptions()
+            print()
+
+        elif menuInput == "o":
+            ShoppingCart.print_total()
+            print()
+
+        elif menuInput == "q":
+            break
+
+        else:
+            print("Invalid entry, please try again")
+            print()
+
+
+def input_item(ItemToPurchase):
+    print("Enter the item name:")
+    ItemToPurchase.item_name = input()
+    print("Enter the item description:")
+    ItemToPurchase.item_description = input()
+    print("Enter the item price:")
+    ItemToPurchase.item_price = float(input())
+    print("Enter the item quantity:")
+    ItemToPurchase.item_quantity = int(input())
+    ItemToPurchase.find_item_total()
 
                 #Define objects of the class ItemToPurchase()
 item01 = ItemToPurchase()
-item02 = ItemToPurchase()
+#item02 = ItemToPurchase()
 cart01 = ShoppingCart()
-itemAddRemove = 'none'
+#testlist = []
+cart01.customer_name = "John Doe"
+cart01.current_date = "February 1, 2020"
+#input_item(item01)
 
+#item01.print_item_cost()
+#print_menu(cart01, item01)
+print_menu(cart01)
+#print(len(cart01.cart_items))
 #print(cart01.cart_items)
 
 #input item01 attributes
-print("Item 1\n")
-print("Enter the item name:\n")
-item01.item_name = input()
-print()
-print("Enter the item price:\n")
-item01.item_price = float(input())
-print()
-print("Enter the item quantity:\n")
-item01.item_quantity = int(input())
-print()
+#print("Item 1")
+#print("Enter the item name:")
+#item01.item_name = input()
+#print("Enter the item description:")
+#item01.item_description = input()
+#print("Enter the item price:")
+#item01.item_price = float(input())
+#print("Enter the item quantity:")
+#item01.item_quantity = int(input())
 
-cart01.add_item(item01)
-print(cart01.cart_items[0].item_name)
-print(cart01.cart_items[0].item_price)
-print(cart01.cart_items[0].item_quantity)
+#input_item(item01)
+#print("Item 1")
+#print("Enter the item name:")
+#item01.item_name = input()
+#print("Enter the item description:")
+#item01.item_description = input()
+#print("Enter the item price:")
+#item01.item_price = float(input())
+#print("Enter the item quantity:")
+#item01.item_quantity = int(input())
+#cart01.add_item(item01)
+#cart01.cart_items.append(item01)
+#testlist.append(item01)
+#input_item(item01)
+#item01 = ItemToPurchase()
+#print("Item 2")
+#print("Enter the item name:")
+#item01.item_name = input()
+#print("Enter the item description:")
+#item01.item_description = input()
+#print("Enter the item price:")
+#item01.item_price = float(input())
+#print("Enter the item quantity:")
+#item01.item_quantity = int(input())
+#cart01.add_item(item01)
+#cart01.cart_items.append(item02)
+#testlist.append(item01)
+#print(cart01.cart_items[0].item_name)
+#print(cart01.cart_items[0].item_description)
+#print(cart01.cart_items[0].item_price)
+#print(cart01.cart_items[0].item_quantity)
+#print(cart01.cart_items[0].item_total)
+#print(cart01.cart_items[1].item_name)
+#print(cart01.cart_items[1].item_description)
+#print(cart01.cart_items[1].item_price)
+#print(cart01.cart_items[1].item_quantity)
+#print(cart01.cart_items[1].item_total)
 
-itemAddRemove = input('Enter Item to Remove: ')
-cart01.remove_item(itemAddRemove)
-cart01.get_num_items_in_cart()
-print('Items in cart: ', cart01.cart_qty)
-cart01.get_cost_of_cart()
-print('Cost of cart: ', cart01.cart_total)
+#print(testlist[0].item_name)
+#print(testlist[0].item_description)
+#print(testlist[0].item_price)
+#print(testlist[0].item_quantity)
+#print(testlist[0].item_total)
+#print(testlist[1].item_name)
+#print(testlist[1].item_description)
+#print(testlist[1].item_price)
+#print(testlist[1].item_quantity)
+#print(testlist[1].item_total)
+
+#cart01.print_total()
+#cart01.print_descriptions()
+
+#itemAddRemove = 'none'
+#itemAddRemove = input('Enter Item to Remove: ')
+#cart01.remove_item(itemAddRemove)
+#cart01.get_num_items_in_cart()
+#print('Items in cart: ', cart01.cart_qty)
+#cart01.get_cost_of_cart()
+#print('Cost of cart: ', cart01.cart_total)
 
 
 
 #input item02 attributes
-print("Item 2\n")
-print("Enter the item name:\n")
-item02.item_name = input()
-print()
-print("Enter the item price:\n")
-item02.item_price = float(input())
-print()
-print("Enter the item quantity:\n")
-item02.item_quantity = int(input())
-print()
+#print("Item 2\n")
+#print("Enter the item name:\n")
+#item02.item_name = input()
+#print()
+#print("Enter the item price:\n")
+#item02.item_price = float(input())
+#print()
+#print("Enter the item quantity:\n")
+#item02.item_quantity = int(input())
+#print()
 
-cart01.modify_item(item02)
+#cart01.modify_item(item02)
 
-print(cart01.cart_items[0].item_name)
-print(cart01.cart_items[0].item_price)
-print(cart01.cart_items[0].item_quantity)
+#print(cart01.cart_items[0].item_name)
+#print(cart01.cart_items[0].item_price)
+#print(cart01.cart_items[0].item_quantity)
 
 
 
